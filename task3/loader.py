8b76663608f0eab97ab38cef6d66bd80a9cf1b57
napronald <113805483+napronald@users.noreply.github.com>
2023-07-11 14:11:24 -0700

    Update loader.py

--- task3/loader.py
+++ task3/loader.py
@@ -1,5 +1,6 @@
 import glob, re, os
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from typing import List
 from cardiac_ml_tools import read_data_dirs, get_standard_leads, get_activation_time
@@ -14,58 +15,42 @@ file_pairs = read_data_dirs(data_dirs)
 
 case = 16116 #16117
 
+
 num_timesteps = 500
 titles = ["I", "II", "III", "aVR", "aVL", "aVF", "V1", "V2", "V3", "V4", "V5", "V6"]
 reorder = {1:1,2:5,3:9,4:2,5:6,6:10,7:3,8:7,9:11,10:4,11:8,12:12} 
 
-print('Case {} : {}'.format(case, file_pairs[case][0]))
-pECGData = np.load(file_pairs[case][0])
-pECGData = get_standard_leads(pECGData) # X-> [500,12]
-
-print(pECGData)
-print(pECGData.shape)
-
-# # create a figure with 12 subplots
-# for i in range(pECGData.shape[1]):
-#     plt.plot(pECGData[0:num_timesteps,i],'r')
-#     plt.title(titles[i])
-#     plt.grid(visible=True, which='major', color='#666666', linestyle='-')
-#     plt.minorticks_on()
-#     plt.grid(visible=True, which='minor', color='#999999', linestyle='-', alpha=0.2)
-#     plt.xlabel('msec')
-#     plt.ylabel('mV')
-# plt.tight_layout()
-# plt.show()
-# # close
-# plt.close()
 
 
-# make a plot with the "pECGData" -> "ActTime"
-print('Case {} : {}'.format(case, file_pairs[case][0]))
-VmData = np.load(file_pairs[case][1])
-ActTime = get_activation_time(VmData)
+# for case in range(16116):
+#     # print('Case {} : {}'.format(case, file_pairs[case][0]))
+#     pECGData = np.load(file_pairs[case][0])
+#     pECGData = get_standard_leads(pECGData) # X-> [500,12]
 
-print(ActTime)
-print(ActTime.shape)
+#     # print(pECGData)
+#     print(pECGData.shape)
 
 
-# # plot in row the tensors pECGData and ActTime with an arrow pointing to the activation time
-# row = 1
-# column = 3
-# plt.figure(figsize=(20, 5))
-# plt.subplot(row, column, 1)
-# # plot pECGData transposed
-# plt.imshow(pECGData.T, cmap='jet', interpolation='nearest', aspect='auto')
-# plt.title('pECGData')
-# plt.subplot(row, column, 2)
-# # print an arrow
-# plt.text(0.5, 0.5, '-------->', horizontalalignment='center', verticalalignment='center', fontsize=20)
-# plt.axis('off')
-# plt.subplot(row, column, 3)
-# # plot ActTime
-# plt.imshow(ActTime, cmap='jet', interpolation='nearest', aspect='auto')
-# # not xticks
-# plt.xticks([])
-# plt.title('ActTime')
-# plt.show()
-# plt.close()
+#     print('Case {} : {}'.format(case, file_pairs[case][0]))
+#     VmData = np.load(file_pairs[case][1])
+#     ActTime = get_activation_time(VmData) # A->[75,1]
+
+#     # print(ActTime)
+#     print(ActTime.shape)
+
+
+dataset = []
+
+for case in range(16116):
+    pECGData = np.load(file_pairs[case][0])
+    pECGData = get_standard_leads(pECGData)
+
+    VmData = np.load(file_pairs[case][1])
+    ActTime = get_activation_time(VmData)
+
+    dataset.append([pECGData, ActTime])
+
+
+df = pd.DataFrame(dataset, columns=["Feature", "Label"])
+
+df.to_csv(os.path.join(os.getcwd(), "dataset.csv"), index=False)

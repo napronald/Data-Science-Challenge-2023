46251b241dc7a6f2433601c63ab54a7bfc8ae294
napronald <113805483+napronald@users.noreply.github.com>
2023-07-11 19:32:18 -0700

    Update train.py

--- task3/train.py
+++ task3/train.py
@@ -18,40 +18,68 @@ from cardiac_ml_tools import read_data_dirs, get_standard_leads, get_activation_
 
 data_dirs = []
 regex = r'data_hearts_dd_0p2*'
-DIR='/home/rnap/scratch/dsc/task3/intracardiac_dataset/' # This should be the path to the intracardiac_dataset, it can be downloaded using data_science_challenge_2023/download_intracardiac_dataset.sh
+DIR='/home/rnap/scratch/dsc/task3/intracardiac_dataset/' 
 for x in os.listdir(DIR):
     if re.match(regex, x):
         data_dirs.append(DIR + x)
 file_pairs = read_data_dirs(data_dirs)
 
 
-
 batch_size = 64
 torch.manual_seed(42)
 
-# for case in range(len(file_pairs)):
-#     pECGData = np.load(file_pairs[case][0])
-#     for lead in range(pECGData.shape[1]):
-#         pECGData[:, lead] = preprocessing.normalize([pECGData[:, lead]])[0]
 
+# class CustomImageDataset(Dataset):
+#     def __init__(self, annotations_file):
+#         self.img_labels = annotations_file
 
+#     def __len__(self):
+#         return len(self.img_labels)
 
+#     def __getitem__(self, idx):
+#         vector = self.img_labels[0]
+#         vector = torch.tensor(vector, dtype=torch.float32)
+
+#         label = self.img_labels[1]
+#         label = torch.tensor(label, dtype=torch.float32)
+        
+#         return vector, label
 
 class CustomImageDataset(Dataset):
-    def __init__(self, annotations_file):
-        self.img_labels = annotations_file
+    def __init__(self, features, labels):
+        self.features = features
+        self.labels = labels
 
     def __len__(self):
-        return len(self.img_labels)
+        return len(self.features)
 
     def __getitem__(self, idx):
-        vector = self.img_labels[0]
-        vector = torch.tensor(vector, dtype=torch.float32)
+        feature = self.features[idx]
+        label = self.labels[idx]
 
-        label = self.img_labels[1]
-        label = torch.tensor(label, dtype=torch.float32)
-        
-        return vector, label
+        return feature, label
+
+
+# class CustomImageDataset(Dataset):
+#     def __init__(self, annotations_file):
+#         self.img_labels = annotations_file
+#         self.transform = transforms.Compose([
+#             transforms.ToTensor(),
+#             transforms.Normalize(mean=[0.5], std=[0.5])
+#         ])
+
+#     def __len__(self):
+#         return len(self.img_labels)
+
+#     def __getitem__(self, idx):
+#         vector = self.img_labels[idx][0]
+#         vector = self.transform(vector)
+
+#         label = self.img_labels[idx][1]
+#         label = self.transform(label)
+
+#         return vector, label
+
 
 
 feature = []
@@ -67,27 +95,27 @@ for case in range(len(file_pairs)):
     VmData = np.load(file_pairs[case][1])
     VmData = get_activation_time(VmData)
 
-    # for i in range(VmData.shape[1]):
-        # VmData[:, i] = preprocessing.normalize([VmData[:, i]])[0]
+    feature.append(torch.tensor(pECGData))
+    label.append(torch.tensor(VmData))
 
-    feature.append(pECGData)
-    label.append(VmData)
 
-data = [feature, label]
-# data = [pECGData, VmData]
-# data = [ [[0, 1, 2], [3, 4, 5]], [[6, 7, 8], [9, 10, 11]] ]
-cid = CustomImageDataset(data)
+cid = CustomImageDataset(feature, label)
 
-print(cid[0][0][0].shape)
-print(cid[0][0][0])
+print(cid[0][0].shape)
 
-print(cid[0][1][0].shape)
-print(cid[0][1][0])
+print(cid[0][1].shape)
 
 data_loader = torch.utils.data.DataLoader(
     cid,
-    batch_size=batch_size,
+    batch_size=2,
     shuffle=True,
     drop_last=True,
     num_workers=0,
 )
+
+for feature, label in data_loader:
+    # print(feature[0][0].shape)
+    # print(label[0][1].shape)
+    print(feature.shape)
+    print(label.shape)
+    break

34252b63c71b21fda224526d97dbcf4fa9222e77
napronald <113805483+napronald@users.noreply.github.com>
2023-07-12 14:51:01 -0700

    Update train.py

--- task3/train.py
+++ task3/train.py
@@ -27,10 +27,45 @@ file_pairs = read_data_dirs(data_dirs)
 batch_size = 64
 torch.manual_seed(42)
 
+# class CustomDataset(Dataset):
+#     def __init__(self, features, labels):
+#         self.features = features
+#         self.labels = labels
+
+#     def __len__(self):
+#         return len(self.features)
+
+#     def __getitem__(self, idx):
+#         feature = self.features[idx]
+#         label = self.labels[idx]
+
+#         return feature, label
+from torchvision import transforms
+
+# class CustomDataset(Dataset):
+#     def __init__(self, features, labels):
+#         self.features = features
+#         self.labels = labels
+#         self.transform = transforms.Compose([transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])])
+
+#     def __len__(self):
+#         return len(self.features)
+
+#     def __getitem__(self, idx):
+#         feature = self.features[idx]
+#         label = self.labels[idx]
+
+#         transformed_feature = self.transform(feature)
+
+#         return transformed_feature, label
+
 class CustomDataset(Dataset):
     def __init__(self, features, labels):
         self.features = features
         self.labels = labels
+        self.transform = transforms.Compose([
+            transforms.Normalize(mean=[0.5], std=[0.5])
+        ])
 
     def __len__(self):
         return len(self.features)
@@ -39,8 +74,10 @@ class CustomDataset(Dataset):
         feature = self.features[idx]
         label = self.labels[idx]
 
-        return feature, label
+        transformed_feature = self.transform(feature.unsqueeze(0))
 
+        return transformed_feature, label
+    
 
 feature = []
 label = []
@@ -49,8 +86,8 @@ for case in range(len(file_pairs)):
     pECGData = np.load(file_pairs[case][0])
     pECGData = get_standard_leads(pECGData)
 
-    for lead in range(pECGData.shape[1]):
-        pECGData[:, lead] = preprocessing.normalize([pECGData[:, lead]])[0]
+    # for lead in range(pECGData.shape[1]):
+    #     pECGData[:, lead] = preprocessing.normalize([pECGData[:, lead]])[0]
     
     VmData = np.load(file_pairs[case][1])
     VmData = get_activation_time(VmData)
@@ -61,6 +98,8 @@ for case in range(len(file_pairs)):
 
 cid = CustomDataset(feature, label)
 
+print(cid[0][0])
+print(cid[1][0])
 
 data_loader = torch.utils.data.DataLoader(
     cid,
@@ -102,42 +141,16 @@ class AE(torch.nn.Module):
             nn.Linear(128, output_dim)
         )
 
-        # self.dense = nn.Sequential(
-        #     nn.Linear(6000, 512),
-        #     nn.ReLU(),
-        #     nn.Linear(512, 256),
-        #     nn.ReLU(),
-        #     nn.Linear(256, 75),
-        #     nn.ReLU(),
-        #     nn.Linear(75, 1)
-        # )
- 
-    # def forward(self, x):
-    #     print(x.shape)
-    #     encoded = self.encoder(x)
-    #     print(encoded.shape) # 64x500x9
-    #     # decoded = self.decoder(encoded)
-    #     # decoded = self.decoder(encoded).reshape(-1, self.output_dim)
-    #     decoded = self.decoder(encoded)
-    #    # print(decoded.reshape(-1, self.output_dim).shape)
-
-    #     print(decoded.shape)
-    #     return decoded
 
     def forward(self, x):
         encoded = self.encoder(x)
         print(encoded.shape)
         decoded = self.decoder(encoded)
         print(decoded.shape)
-        decoded = decoded.view(decoded.size(0), -1, self.output_dim)
-        print(decoded.view(decoded.size(0), -1, self.output_dim).shape)
-        output = self.dense(decoded)
-        print(output.shape)
-        # return decoded
-        return output
+        return decoded
 
 
-model = AE(input_dim=500*12, hidden_dim=128, output_dim=75)
+model = AE(input_dim=6000, hidden_dim=128, output_dim=75)
 model = model.double()
 
 criterion = nn.MSELoss()
@@ -151,7 +164,8 @@ for epoch in range(1, num_epochs):
     for step, (feature, label) in enumerate(data_loader):
         label = label.float()
         optimizer.zero_grad()
-        logits = model(feature)
+        logits = model(feature.unsqueeze(1))
+
         print(logits.shape)
         print(logits.unsqueeze(2))
         print(label.shape)

8ab918b1765b23075dc41611623e14a1ff63219c
napronald <113805483+napronald@users.noreply.github.com>
2023-07-13 20:45:49 -0700

    LSTM + CONV1D

--- task3/train.py
+++ task3/train.py
@@ -50,153 +50,85 @@ for case in range(len(file_pairs)):
         
     VmData = np.load(file_pairs[case][1])
     VmData = get_activation_time(VmData)
-
-    for i in range(12):
-        print(pECGData[:, i].shape)
-        print(pECGData[:, i])
-
-        min = np.min(pECGData[:,i])
-        max = np.max(pECGData[:,i])
-        print(min)
-        print(max)
-
-        pECGData[:,i] = (pECGData[:,i] - min) / (max-min)
-        print(pECGData[:,i].shape)
-        std = np.std(pECGData)
-        mean = np.mean(pECGData)
-        # print(std)
-        # print(mean)
-
-    # VmData = ( ( (pECGData - mean) / std) + 1) / 2
-
+    
     label.append(torch.tensor(VmData))
     feature.append(np.array(pECGData))
     
 feature = np.array(feature)
-
-# for lead in range(12):
-#     lead_data = feature[:][:][lead]
-#     mean = np.mean(lead_data)
-#     std = np.std(lead_data)
-#     feature[:][:][lead] = ( ( (lead_data - mean) / std) + 1) / 2
-
-
-for i in range(12):
-    feature[:][i][:] = ( (x[:][i][:] - np.min(feature[:][i][:])) / (np.max(feature[:][i][:]) - np.min(feature[:][i][:])) )
     
-    print(feature.shape)
-    # mean = np.mean(lead_data)
-    # std = np.std(lead_data)
-    # feature[:][:][lead] = ( ( (lead_data - mean) / std) + 1) / 2
+lead_data_min = np.min(np.min(feature, axis=1), axis=0)
+lead_data_min = lead_data_min.reshape(1, 1, -1)
 
-# feature = list(feature)
-# for i in range(len(feature)):
-#     feature[i] = torch.tensor(feature[i])
+lead_data_max = np.max(np.max(feature, axis=1), axis=0)
+lead_data_max = lead_data_max.reshape(1, 1, -1)
+
+feature = (feature - lead_data_min) / (lead_data_max - lead_data_min)
+
+# label = np.array(label)
+
+# act_data_min = np.min(np.min(label, axis=1), axis=0)
+# act_data_min = act_data_min.reshape(1, 1, -1)
+
+# act_data_max = np.max(np.max(label, axis=1), axis=0)
+# act_data_max = act_data_max.reshape(1, 1, -1)
+
+# label = (feature - act_data_min) / (act_data_max - act_data_min)
+
+feature = list(feature)
+for i in range(len(feature)):
+    feature[i] = torch.tensor(feature[i], dtype=torch.float32)
+
+# label = list(label)    
+
+# torch.tensor(feature)
     
-
 cid = CustomDataset(feature, label)
-
-
-for index in range(600):
-    mean_value = np.mean(cid[index][0])
-    print("Mean:", mean_value)
-    
-for index in range(600):
-    std_value = np.std(cid[index][0])
-    print("Standard Deviation:", std_value)
 
 data_loader = torch.utils.data.DataLoader(
     cid,
-    batch_size=batch_size,
+    batch_size=64,
     shuffle=True,
     drop_last=True,
     num_workers=0,
 )
 
 
-class EncoderRNN(nn.Module):
-    def __init__(self, input_dim, hidden_dim, layer_dim, output_dim):
-        super().__init__()
+class LSTMCNN(nn.Module):
+    def __init__(self, input_dim, hidden_dim, layer_dim):
+        super(LSTMCNN, self).__init__()
         self.hidden_dim = hidden_dim
         self.layer_dim = layer_dim
         self.rnn = nn.LSTM(input_dim, hidden_dim, layer_dim, batch_first=True)
-        self.fc = nn.Linear(hidden_dim, output_dim)
-
+        self.cnn = nn.Conv1d(in_channels=1, out_channels=1, kernel_size=3)
+ 
     def forward(self, x):
         out, (hn, cn) = self.rnn(x)
-        out = self.fc(hn[-1])
-        return out, hn
+        print(out.shape)
+        out = out.unsqueeze(1)
+        decoded = self.cnn(out)
+        return decoded
 
-MAX_LENGTH = 100
-class DecoderRNN(nn.Module):
-    def __init__(self, hidden_size, output_size):
-        super(DecoderRNN, self).__init__()
-        self.embedding = nn.Embedding(output_size, hidden_size)
-        self.gru = nn.GRU(hidden_size, hidden_size, batch_first=True)
-        self.out = nn.Linear(hidden_size, output_size)
+model = LSTMCNN(input_dim=6000, hidden_dim=77, layer_dim=5)
 
-    def forward(self, encoder_outputs, encoder_hidden, target_tensor=None):
-        batch_size = encoder_outputs.size(0)
-        decoder_input = torch.empty(batch_size, 1, dtype=torch.long).fill_(0)
-        decoder_hidden = encoder_hidden
-        decoder_outputs = []
+criterion = nn.MSELoss()
 
-        for i in range(MAX_LENGTH):
-            decoder_output, decoder_hidden  = self.forward_step(decoder_input, decoder_hidden)
-            decoder_outputs.append(decoder_output)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
-            if target_tensor is not None:
-                decoder_input = target_tensor[:, i].unsqueeze(1) 
-            else:
-                _, topi = decoder_output.topk(1)
-                decoder_input = topi.squeeze(-1).detach()  
-
-        decoder_outputs = torch.cat(decoder_outputs, dim=1)
-        decoder_outputs = F.log_softmax(decoder_outputs, dim=-1)
-        return decoder_outputs, decoder_hidden, None
-
-    def forward_step(self, input, hidden):
-        output = self.embedding(input)
-        output = F.relu(output)
-        output, hidden = self.gru(output, hidden)
-        output = self.out(output)
-        return output, hidden
-
-
-encoder = EncoderRNN(input=12, hidden_size=50)
-decoder = DecoderRNN(hidden_size=50, output_size=75)
-
-encoder_optimizer = optim.Adam(encoder.parameters(), lr=0.001)
-decoder_optimizer = optim.Adam(decoder.parameters(), lr=0.001)
-
-criterion = nn.NLLLoss()
-
-num_epochs = 100
-
-for epoch in range(1, num_epochs):
-    loss_epoch = 0
+epochs = 10
+for epoch in range(epochs):
     for step, (feature, label) in enumerate(data_loader):
+        feature = feature.reshape(-1, 12*500)
+        feature = feature.to(torch.float32)
 
-        encoder_optimizer.zero_grad()
-        decoder_optimizer.zero_grad()
+        optimizer.zero_grad()
+        print(feature.shape)
+        y_pred = model(feature)
 
-        encoder_outputs, encoder_hidden = encoder(feature)
-        decoder_outputs, _, _ = decoder(encoder_outputs, encoder_hidden, label)
+        label = label.reshape(-1, 75*1)
+        label = label.float()
+        loss = criterion(150*y_pred, label)
 
-        loss = criterion(
-            decoder_outputs.view(-1, decoder_outputs.size(-1)),
-            label.view(-1)
-        )
         loss.backward()
+        optimizer.step()
 
-        encoder_optimizer.step()
-        decoder_optimizer.step()
-
-        loss_epoch += loss.item()
-
-        if step % 250 == 0:
-            print(
-                f"Step [{step}/{len(data_loader)}]\t loss: {loss.item()}")
-        loss_epoch += loss.item()
-
-    print(f"Epoch [{epoch}/{num_epochs}]\t Loss: {loss_epoch / len(data_loader)}")
+    print(f'Epoch: {epoch} {loss.item()}')

f94d3a014b7d2cb8b1c94cd69ace230e2430e974
napronald <113805483+napronald@users.noreply.github.com>
2023-07-17 17:45:05 -0700

    removed pooling + unnormalize + scheduler + reduction=sum

--- task3/train.py
+++ task3/train.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 from torch.utils.data import Dataset
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 from sklearn.metrics import r2_score
 import glob, re, os
 import numpy as np
@@ -21,7 +22,7 @@ for x in os.listdir(DIR):
 file_pairs = read_data_dirs(data_dirs)
 
 
-batch_size = 64
+batch_size = 512
 torch.manual_seed(42)
 
 class CustomDataset(Dataset):
@@ -52,9 +53,8 @@ for case in range(len(file_pairs)):
     
     label.append(torch.tensor(VmData))
     feature.append(np.array(pECGData))
-        
-feature = np.array(feature)
-    
+
+
 lead_data_min = np.min(np.min(feature, axis=1), axis=0)
 lead_data_min = lead_data_min.reshape(1, 1, -1)
 
@@ -63,145 +63,141 @@ lead_data_max = lead_data_max.reshape(1, 1, -1)
 
 feature = (feature - lead_data_min) / (lead_data_max - lead_data_min)
 
-# label = np.array(label)
+act_data_min = torch.min(torch.min(torch.stack(label, dim=0), dim=1).values, dim=0).values
+act_data_min = act_data_min.reshape(1, 1, -1)
 
-# act_data_min = np.min(np.min(label, axis=1), axis=0)
-# act_data_min = act_data_min.reshape(1, 1, -1)
+act_data_max = torch.max(torch.max(torch.stack(label, dim=0), dim=1).values, dim=0).values
+act_data_max = act_data_max.reshape(1, 1, -1)
 
-# act_data_max = np.max(np.max(label, axis=1), axis=0)
-# act_data_max = act_data_max.reshape(1, 1, -1)
+label = [(l - act_data_min) / (act_data_max - act_data_min) for l in label]
 
-# label = (feature - act_data_min) / (act_data_max - act_data_min)
+feature = [torch.tensor(f, dtype=torch.float32) for f in feature]
+label = [torch.tensor(l, dtype=torch.float32) for l in label]
 
-feature = list(feature)
-for i in range(len(feature)):
-    feature[i] = torch.tensor(feature[i], dtype=torch.float32)
 
-# label = list(label)    
 
-# torch.tensor(feature)
-    
-cid = CustomDataset(feature, label)
+cid = CustomDataset(feature[:12894], label[:12894])
 
-data_loader = torch.utils.data.DataLoader(
+train_data = torch.utils.data.DataLoader(
     cid,
-    batch_size=512,
+    batch_size=batch_size,
     shuffle=True,
     drop_last=True,
     num_workers=0,
 )
 
+cid = CustomDataset(feature[12894:], label[12894:])
 
-# class CNN(nn.Module):
-#     def __init__(self):
-#         super(CNN, self).__init__()
-#         self.model = nn.Sequential(
-#             nn.Conv1d(12, 75, kernel_size=500),
-#             nn.BatchNorm1d(75),
-#             nn.ReLU(),
-#             nn.Sigmoid()
-#         )
-
-#     def forward(self, x):
-#         decoded = self.model(x)
-#         # print(decoded.shape)
-#         return decoded
-
-
-# model = CNN()
-
-# class SqueezeNet1D(nn.Module):
-#     def __init__(self, output_dim):
-#         super(SqueezeNet1D, self).__init__()
-#         self.features = nn.Sequential(
-#             nn.Conv1d(12, 64, kernel_size=1),
-#             nn.ReLU(inplace=True),
-#             nn.MaxPool1d(kernel_size=2, stride=2),
-#             nn.Conv1d(64, 16, kernel_size=1),
-#             nn.ReLU(inplace=True),
-#             nn.Conv1d(16, 16, kernel_size=1),
-#             nn.ReLU(inplace=True),
-#             nn.MaxPool1d(kernel_size=2, stride=2),
-#             nn.Conv1d(16, output_dim, kernel_size=1),
-#             nn.ReLU(inplace=True),
-#         )
-#         self.classifier = nn.Sequential(
-#             nn.AdaptiveAvgPool1d(1),
-#             nn.Flatten(),
-#         )
-
-#     def forward(self, x):
-#         x = self.features(x)
-#         x = self.classifier(x)
-#         return x
-
-# model = SqueezeNet1D(output_dim=75)
-
-# criterion = nn.MSELoss()
-
-# optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-
-# epochs = 250
-# for epoch in range(epochs):
-#     for step, (feature, label) in enumerate(data_loader):
-#         feature = feature.to(torch.float32)
-#         # print(feature.shape)
-#         optimizer.zero_grad()
-#         y_pred = model(feature.permute(0, 2, 1))
-#         y_pred = y_pred.unsqueeze(2)
-#         # print(y_pred.shape)
-#         # print(label.shape)
-#         label = label.float()
-#         loss = criterion(y_pred, label/200)
-#         loss.backward()
-#         optimizer.step()
-#         y_pred_np = y_pred.detach().numpy().reshape(-1)
-#         label_np = label.detach().numpy().reshape(-1)
-#         r2 = r2_score(label_np/200, y_pred_np)
-#     print(r2)
-
-#     print(f'Epoch: {epoch} loss: {loss.item()}')
-
+test_data = torch.utils.data.DataLoader(
+    cid,
+    batch_size=batch_size,
+    shuffle=True,
+    drop_last=True,
+    num_workers=0,
+)
 
 class SqueezeNet1D(nn.Module):
     def __init__(self, output_dim):
         super(SqueezeNet1D, self).__init__()
 
         self.model = nn.Sequential(
-            nn.Conv1d(12, 512, kernel_size=1),
+            nn.Conv1d(500, 512, kernel_size=3),
+            nn.BatchNorm1d(512),
             nn.ReLU(inplace=True),
-            nn.MaxPool1d(kernel_size=2, stride=2),
-            nn.Conv1d(512, 256, kernel_size=1),
+            # nn.MaxPool1d(kernel_size=1, stride=2),
+            nn.Conv1d(512, 256, kernel_size=3),
             nn.BatchNorm1d(256),
             nn.ReLU(inplace=True),
-            nn.Conv1d(256, 128, kernel_size=1),
-            nn.BatchNorm1d(128), 
+            nn.Conv1d(256, 128, kernel_size=3),
+            nn.BatchNorm1d(128),
             nn.ReLU(inplace=True),
-            nn.MaxPool1d(kernel_size=2, stride=2),
-            nn.Conv1d(128, output_dim, kernel_size=1),
+            # nn.MaxPool1d(kernel_size=1, stride=2),
+            nn.Conv1d(128, 75, kernel_size=3),
+            nn.BatchNorm1d(75),
             nn.ReLU(inplace=True),
-            nn.Sigmoid()
         )
+        self.avg_pool = nn.AdaptiveAvgPool1d(1)  
+        self.fc = nn.Linear(75, output_dim) 
+        self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
         x = self.model(x)
+        # print(x.shape)
+        x = self.avg_pool(x)
+        # print(x.shape)
+        x = x.view(x.size(0), -1)
+        # print(x.shape)
+        x = self.fc(x)
+        # print(x.shape)
+        x = self.sigmoid(x)
+        # print(x.shape)
         return x
+
 
 model = SqueezeNet1D(output_dim=75)
 
-criterion = nn.MSELoss()
+criterion = nn.MSELoss(reduction='sum')
 
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=3, verbose=True)
 
-epochs = 100
+epochs = 50
 for epoch in range(epochs):
-    for step, (feature, label) in enumerate(data_loader):
+    err = 0
+    avg_error = []
+    for step, (feature, label) in enumerate(train_data):
         feature = feature.to(torch.float32)
         optimizer.zero_grad()
-        y_pred = model(feature.permute(0, 2, 1))
+        y_pred = model(feature)
         label = label.float()
-        loss = criterion(y_pred, label/185)
+        loss = criterion(y_pred.unsqueeze(2), label.squeeze(1))
         loss.backward()
         optimizer.step()
 
+        sum=0
+
+        y_pred = y_pred.unsqueeze(2)
+        label = label.squeeze(1)
+
+        y_pred = y_pred * (act_data_max - act_data_min) + act_data_min
+        label = label * (act_data_max - act_data_min) + act_data_min
+        print(y_pred[0])
+        print(label[0])
+        for i in range(batch_size):
+            diff = (y_pred[i, :, :] - label[i, :, :])
+            sum += torch.sqrt(torch.norm(diff, p=2))
+        err = sum/batch_size
+        avg_error.append(err)
+
+    scheduler.step(loss)
+    avg_error = torch.tensor(avg_error)
+    print(torch.sum(avg_error)/len(avg_error))
     print(f'Epoch: {epoch} Loss: {loss.item()}')
+
+
+model.eval()
+err = 0
+avg_error = []
+with torch.no_grad():
+    for feature, label in test_data:
+        feature = feature.to(torch.float32)
+        label = label.float()
+
+        y_pred = model(feature)
+        label = label.squeeze(1)
+        y_pred = y_pred.unsqueeze(2)
+
+        y_pred = y_pred * (act_data_max - act_data_min) + act_data_min
+        label = label * (act_data_max - act_data_min) + act_data_min
+
+        sum=0
+
+        for i in range(batch_size):
+            diff = (y_pred[i, :, :] - label[i, :, :])
+            sum += torch.sqrt(torch.norm(diff, p=2))
+        err = sum/batch_size
+        print(err)
+        avg_error.append(err)
+
+    print(sum(avg_error)/len(avg_error))
+    
